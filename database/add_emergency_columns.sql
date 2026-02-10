85f5c35122c1be24eec214e577538a9730186d64
Abimanyu Danendra A <132000257+AbimanyuDA@users.noreply.github.com>
2026-02-10 16:33:34 +0700

    Delete database directory

--- database/add_emergency_columns.sql
+++
@@ -1,6 +0,0 @@
--- Add emergency_reason and emergency_type columns to SCHEDULES table
-ALTER TABLE SCHEDULES ADD emergency_reason VARCHAR2(1000);
-ALTER TABLE SCHEDULES ADD emergency_type VARCHAR2(20);
-
--- Commit changes
-COMMIT;
